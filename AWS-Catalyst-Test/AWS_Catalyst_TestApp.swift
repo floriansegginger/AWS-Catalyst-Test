51347095dd326d85eaf789efa16855d1dd6d4a42
Florian Segginger <florian.segginger@screeningeagle.com>
2023-05-09 15:23:28 +0200

    Initial Commit

---
+++ AWS-Catalyst-Test/AWS_Catalyst_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWS_Catalyst_TestApp.swift
+//  AWS-Catalyst-Test
+//
+//  Created by Florian Segginger (local) on 09.05.23.
+//
+
+import SwiftUI
+
+@main
+struct AWS_Catalyst_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AWS-Catalyst-Test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AWS-Catalyst-Test
+//
+//  Created by Florian Segginger (local) on 09.05.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
